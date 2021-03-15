d7c5cffbffc081c4ee3e6888cbcad3243747480a
Laurynas Butkus <laurynas.butkus@gmail.com>
2021-03-15 23:04:57 +0200

    Add long description

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@
 from setuptools.depends import get_module_constant
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name = 'detectlanguage',
     packages = ['detectlanguage'],
     version = get_module_constant('detectlanguage', '__version__'),
     description = 'Language Detection API Client',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author = 'Laurynas Butkus',
     author_email = 'info@detectlanguage.com',
     url = 'https://github.com/detectlanguage/detectlanguage-python',
