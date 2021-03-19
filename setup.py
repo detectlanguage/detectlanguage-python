3a3b2b40191bda7acebf084aece8e6f5ed000db4
Laurynas Butkus <laurynas.butkus@gmail.com>
2021-03-19 19:16:09 +0200

    Fix long description loading on python 2.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools.depends import get_module_constant
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
